18f7958b7389a3245527eaa515381ca02ca12ad6
Sayeed-titan <sayeed1286265@gmail.com>
2025-09-03 22:28:27 +0600

    Add viewmodels

---
+++ CoreSPA/ViewModels/FeatureVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreSPA.ViewModels
+{
+    public class FeatureVM
+    {
+        public int FeatureId { get; set; }
+
+        [Required]
+        [StringLength(150, ErrorMessage = "Feature name cannot exceed 150 characters")]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ CoreSPA/ViewModels/ProductVM.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreSPA.ViewModels
+{
+    public class ProductVM
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        [StringLength(100)]
+        public string? Brand { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public string? Photo { get; set; }
+
+        public IFormFile? PhotoFile { get; set; }
+
+        public List<FeatureVM> Features { get; set; } = new();
+    }
+}

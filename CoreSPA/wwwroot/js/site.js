1414a8362526056b1ca89d3fec68f5a55fbe9c34
Sayeed-titan <sayeed1286265@gmail.com>
2025-09-06 03:05:18 +0600

    Add sctipts to function confirm dialog and toast

--- CoreSPA/wwwroot/js/site.js
+++ CoreSPA/wwwroot/js/site.js
@@ -1,4 +1,47 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.UI = (function () {
 
-// Write your JavaScript code.
+    // Show confirmation modal
+    function confirm(message, callback) {
+        const modalEl = document.getElementById('confirmModal');
+        if (!modalEl) {
+            console.error('Confirm modal element not found!');
+            return;
+        }
+
+        $('#confirmModal .modal-body').text(message);
+        const modal = new bootstrap.Modal(modalEl);
+        modal.show();
+
+        $('#confirmYesBtn').off('click').on('click', function () {
+            callback();
+            modal.hide();
+        });
+    }
+
+    // Show toast notification
+    function toast(message, type = 'success') {
+        const toastEl = $('#actionToast');
+        if (!toastEl.length) {
+            console.error('Toast element not found!');
+            return;
+        }
+
+        toastEl.removeClass('bg-success bg-danger bg-info bg-warning')
+            .addClass({
+                success: 'bg-success',
+                error: 'bg-danger',
+                info: 'bg-info',
+                warning: 'bg-warning'
+            }[type]);
+
+        toastEl.find('.toast-body').text(message);
+        const toastInstance = new bootstrap.Toast(toastEl[0], { delay: 2500 });
+        toastInstance.show();
+    }
+
+    return {
+        confirm,
+        toast
+    };
+
+})();
